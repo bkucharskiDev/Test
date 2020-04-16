d33ac367da599d150727aeba09852f9aacafae4d
Bartosz Kucharski <bartosz.kucharski@netguru.com>
2020-04-16 12:38:14 +0200

    SAVEPOINT

---
+++ Test/XD.swift
@@ -0,0 +1,21 @@
+//
+//  XD.swift
+//  Test
+//
+//  Created by Bartosz Kucharski on 16/04/2020.
+//  Copyright © 2020 Bartosz Kucharski. All rights reserved.
+//
+
+import Foundation
+
+
+class XD {
+    
+    func foo() {
+        print()
+        [1,2,3,4,5]
+        print("TESTSSSSSS")
+    }
+    
+    // TODO
+}
